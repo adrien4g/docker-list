95ad632ae406591b6d4631269a4aff50c1c17eb3
Adrien <adrien.de.adrien@gmail.com>
2021-10-09 23:43:36 -0300

    Update util functions

--- app/docker_conf.py
+++ app/docker_conf.py
@@ -1,6 +1,8 @@
 import docker
-from .utils import *
+from .utils import Utils
 from configparser import ConfigParser
+
+utils = Utils()
 
 class DockerList:
     data = []
@@ -10,15 +12,15 @@ class DockerList:
         self.client = docker.APIClient(base_url='unix://var/run/docker.sock')
         self.config = ConfigParser()
         
-        self.config.read('src/config.ini')
+        self.config.read('app/config.ini')
 
     def get_data(self):
-        verify_directorys()
+        utils.verify_directorys()
         print('Getting list of containers...')
         container_list = self.dockerClient.containers.list()
         progress_counter = 1
         for container in container_list:
-            progress(progress_counter, len(container_list))
+            utils.progress(progress_counter, len(container_list))
             selected_container = self.client.inspect_container(container.id)
             if self.check_for_docker_compose(selected_container):
                 self.container_data = {
@@ -35,15 +37,16 @@ class DockerList:
                         vol["Source"],
                         vol["Destination"]
                     ])
-                    self.container_data.volume_size.append(calculate_files_size(vol["Source"]))
+                    self.container_data.volume_size.append(utils.calculate_files_size(vol["Source"]))
 
                 self.data.append(self.container_data)
             progress_counter += 1
 
         new_data = sorted(self.data, key=lambda x: x['docker_compose'])
         if len(new_data) != 0:
-            create_csv(new_data)
-    
+            utils.create_csv(new_data)
+        else:
+            print('\nNo containers were found')
     def check_for_docker_compose(self, container):
         try:
             path = container['Config']['Labels']["com.docker.compose.project.working_dir"]
